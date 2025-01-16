d34c9062737f60e10692c0f015e1e2d1b6466cea
Gerard Paris <gerard.paris@gmail.com>
2025-01-16 12:12:29 +0100

    Added viewTransactions endpoint

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -5,6 +5,7 @@ var db = require('../database');
 router.get("/viewAccounts", async function (req, res, next) {
 
     try {
+
         const card = await db.Card.findOne({
             where: { number: req.query.cardNumber },
             include: {
@@ -16,6 +17,8 @@ router.get("/viewAccounts", async function (req, res, next) {
         if (!card) {
             throw new Error('Card not found');
         }
+
+        // TODO verify PIN number
 
         const personId = card.Account.Person.id;
 
@@ -31,8 +34,42 @@ router.get("/viewAccounts", async function (req, res, next) {
 
 });
 
-router.get("/viewTransactions", function (req, res) {
-    res.send('not implemented');
+router.get("/viewTransactions", async function (req, res, next) {
+    try {
+
+        const card = await db.Card.findOne({
+            where: { number: req.query.cardNumber },
+            include: {
+                model: db.Account,
+            },
+        });
+
+        if (!card) {
+            throw new Error('Card not found');
+        }
+
+        // TODO verify PIN number
+
+        // Check if the card is linked to the account with the given IBAN
+        const account = await db.Account.findOne({
+            where: { iban: req.query.iban, id: card.accountId },
+        });
+    
+        if (!account) {
+            throw new Error('No matching account found for the given IBAN and card number');
+        }
+    
+        // Fetch transactions for the account
+        const transactions = await db.Transaction.findAll({
+            where: { accountId: account.id },
+            order: [['createdAt', 'DESC']], // Sort transactions by most recent
+        });
+  
+
+        res.status(200).send(JSON.stringify(transactions));
+    } catch (error) {
+        return next(error);
+    }
 });
 
 router.post("/withdrawMoney", function (req, res) {
